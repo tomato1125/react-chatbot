73769f5f1d99fcdb6aca568f104135ce77795c39
tomato1125 <naokiyoshida1125@icoud.com>
2020-10-30 00:18:47 +0900

    firebase-init

---
+++ .firebase-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyAVv-8TSjgDbnq3C1Oso6mOWjFB0eF8-cw",
+  authDomain: "react-chatbot-2e80c.firebaseapp.com",
+  databaseURL: "https://react-chatbot-2e80c.firebaseio.com",
+  projectId: "react-chatbot-2e80c",
+  storageBucket: "react-chatbot-2e80c.appspot.com",
+  messagingSenderId: "999819616337",
+  appId: "1:999819616337:web:d9121bff04bef9e4f11870"
+};
