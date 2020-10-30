ca53add083343763a56a347528d7718c89962f6c
tomato1125 <naokiyoshida1125@icoud.com>
2020-10-30 16:36:25 +0900

    apply-grid-layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 
-const Main = () => {
-  return <div>Main</div>
+import MessageInputField from './MessageInputField';
+import MessageList from './MessageList';
+
+//material-ui stylesのdocを参考
+const useStyles = makeStyles({
+  root: {
+    display: 'grid',
+    height: '100vh',
+    gridTemplateRows: '1fr auto',
+  },
+});
+
+// nameをMainで受け取るようにする
+const Main = ({ name }) => {
+  const classes = useStyles();
+
+  return (
+    // InputFieldはニックネームを持つ必要があるので、MessageInputFieldにnameを渡しておく
+    <div className={classes.root}>
+      <MessageList />
+      <MessageInputField name={name} /> 
+    </div>
+  );
 };
 
 export default Main;
