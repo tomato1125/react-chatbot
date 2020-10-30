e86269390a50670cd92d9747e8da05dfb5c80a82
tomato1125 <naokiyoshida1125@icoud.com>
2020-10-30 15:16:17 +0900

    set-name-in-japanese

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+import Main from './Main';
 import SignIn from './SignIn';
 
 
@@ -7,6 +8,13 @@ export default () => {
   //useStateを使う。初期値を空文字「('')」とする。状態の名前を「name」とする。
   const [name, setName] = useState('');
   console.log({name});
+  //nameが空だった場合はSignInを返す
+  if (name === '') {
+    return <SignIn setName={setName} />;
+  } else {
+    //nameが空じゃない場合はMainコンポーネントのnameを返す。
+    return <Main name={name} />;
+  }
   //変えたい場所（SignInコンポーネント）にニックネームに文字が入力されたらsetNameを利用して、nameの状態を変える。
   //setNameが更新されると、８行目のnameが更新される。すると、９行目のconsole.logでnameの内容がブラウザに表示される。
   return <SignIn setName={setName} />;
