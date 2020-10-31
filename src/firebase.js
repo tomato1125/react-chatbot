1385d516786ab78b41addc7ffde56b7b90be662b
tomato1125 <naokiyoshida1125@icoud.com>
2020-10-31 10:54:49 +0900

    remove-hard-coded-env-vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase';
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAVv-8TSjgDbnq3C1Oso6mOWjFB0eF8-cw",
-  authDomain: "react-chatbot-2e80c.firebaseapp.com",
-  databaseURL: "https://react-chatbot-2e80c.firebaseio.com",
-  projectId: "react-chatbot-2e80c",
-  storageBucket: "react-chatbot-2e80c.appspot.com",
-  messagingSenderId: "999819616337",
-  appId: "1:999819616337:web:d9121bff04bef9e4f11870"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 // 公式docより参照（イニシャライズを行う）
